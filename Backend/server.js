c71a55c5451a69fbf047174df304cce899ef3bdf
VedantHanda771 <vedanthanda123@gmail.com>
2024-11-28 12:03:05 +0530

    deploying

--- Backend/server.js
+++ Backend/server.js
@@ -26,8 +26,8 @@ if (!SECRET_KEY || !process.env.MONGODB_URL) {
 // MongoDB connection
 mongoose
   .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => {
