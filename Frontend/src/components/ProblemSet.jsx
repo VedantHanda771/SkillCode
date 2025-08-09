6e83cb7df6325810eec8365a0084242f3d6583db
VedantHanda771 <vedanthanda123@gmail.com>
2025-08-09 19:30:55 +0530

    Updated deployment url

--- Frontend/src/components/ProblemSet.jsx
+++ Frontend/src/components/ProblemSet.jsx
@@ -9,7 +9,7 @@ function ProblemSet() {
     useEffect(() => {
         const fetchQuestions = async () => {
             try {
-                const response = await fetch('https://cheatcode-us36.onrender.com/problems');
+                const response = await fetch('https://cheatcode-us36.onrender.com/api/Problems');
                 const data = await response.json();
                 if (response.ok) {
                     setQuestions(data);
