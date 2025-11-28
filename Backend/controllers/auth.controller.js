9129613cf8e26cfed5a5f1a6e6fa687128e80fdf
VedantHanda771 <vedanthanda123@gmail.com>
2025-11-28 23:14:14 +0530

    correctly configed the env

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
+const dotenv = require('dotenv');
+dotenv.config();
 const SECRET_KEY = process.env.JWT_SECRET;
 
 exports.signup = async (req, res) =>{
